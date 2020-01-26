263ba742e89dcfbd69f7960f66b05742c6f17a0a
Marcin Kretkowski <marcinkretkowski747@gmail.com>
2020-01-26 14:39:44 +0100

    change error

--- polySmoof.R
+++ polySmoof.R
@@ -1,14 +1,22 @@
 library(polynom)
+library(ParamHelpers)
+library(checkmate)
 
 source("functions.R")
 
-set.seed(42)
+set.seed(9)
 
-arguments <- 40000000
+arguments <- 40000
 area <- matrix(data = runif(arguments,-10,10), nrow=arguments/2, ncol=2)
 y <- generateAluffiPentini(area)
 
 model <- lm(y ~ poly(area[,1],area[,2], degree = 6, raw=TRUE)) # tu trzeba sporzadzic dataset y x1 x2
-data_outpu = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
-intervals <- predict(model, data_outpu, interval='confidence', level = 0.9)
-MSEerr <- mse(model$residuals)
+train_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
+
+set.seed(210)
+area2 <- matrix(data = runif(arguments,10,20), nrow=arguments/2, ncol=2)
+y2 <- generateAluffiPentini(area)
+test_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y2)
+
+intervals <- predict(model, test_data, interval='confidence', level = 0.9)
+MSEerr <- mse(intervals - test_data$Y)
