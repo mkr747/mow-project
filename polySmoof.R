8fd769d8d4ea82f405b6872548fc79f1c12d431b
Raf2406 <43999472+Raf2406@users.noreply.github.com>
2020-01-27 16:27:23 +0100

    Add files via upload

--- polySmoof.R
+++ polySmoof.R
@@ -1,22 +1,42 @@
+
 library(polynom)
 library(ParamHelpers)
 library(checkmate)
 
 source("functions.R")
 
+#TRAIN
 set.seed(9)
-
-arguments <- 40000
+arguments <- 400000
 area <- matrix(data = runif(arguments,-10,10), nrow=arguments/2, ncol=2)
 y <- generateAluffiPentini(area)
-
-model <- lm(y ~ poly(area[,1],area[,2], degree = 6, raw=TRUE)) # tu trzeba sporzadzic dataset y x1 x2
 train_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
 
-set.seed(210)
-area2 <- matrix(data = runif(arguments,10,20), nrow=arguments/2, ncol=2)
-y2 <- generateAluffiPentini(area)
-test_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y2)
+model <- lm(y~ poly(X1, X2, degree =4, raw=TRUE), data=train_data)
 
-intervals <- predict(model, test_data, interval='confidence', level = 0.9)
-MSEerr <- mse(intervals - test_data$Y)
+intervals <- predict(model, interval='confidence', level = 0.99)
+MSEerr <- mse(intervals - train_data$Y) #maly blad
+
+#TEST
+set.seed(210)
+area2 <- matrix(data = runif(arguments,-10,10), nrow=arguments/2, ncol=2)
+y2 <- generateAluffiPentini(area2)
+test_data = data.frame(X1 = area2[,1], X2 = area2[,2], Y = y2)
+
+intervals_T <- predict(model, newdata = test_data, interval='confidence', level = 0.99)
+MSEerr_T <- mse(intervals_T - test_data$Y) 
+
+summary(model)
+
+#CEC2013
+set.seed(42)
+#d <- 2 #wymiary, zmiennych jest tyle ile wymiarów
+arguments <- 2
+area <- matrix(data = runif(arguments,-30000,30000), nrow=arguments*50, ncol=2)
+y <- generateShiftedAndRotatedAckley(area)
+train_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
+
+model <- lm(y~ poly(X1,X2, degree =10, raw=TRUE), data=train_data)
+
+intervals <- predict(model, interval='confidence', level = 0.99)
+MSEerr <- mse(intervals - train_data$Y) #maly blad
