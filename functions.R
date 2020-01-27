3e43cfa84d024e1291c54b3b68f4b4fe33a1a12b
Marcin Kretkowski <marcinkretkowski747@gmail.com>
2020-01-27 22:06:32 +0100

    mae 2

--- functions.R
+++ functions.R
@@ -8,6 +8,11 @@ mse <- function(error)
   mean(error^2)
 }
 
+mae <- function(error)
+{
+  sum(abs(error))/length(error)
+}
+
 #GOLDSTEIN-PRICE FUNCTION
 #[-2,2] - x range
 #func1 <- function(x1, x2) (1 + (x1 + x2 +1)^2(19-14*x1+3*x2-14*x2+6*x1*x2+3*x2^2)) x (30+(2*x1-3*x2)^2*(18-32*x1+12*x1^2+48*x2-36*x1*x2+27*x2^2))
