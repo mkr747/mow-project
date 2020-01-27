1c9790b3f0a5d53e9a95bfad2e50a7b1de19824c
Raf2406 <43999472+Raf2406@users.noreply.github.com>
2020-01-27 19:28:55 +0100

    Add files via upload
    Posprzątane, stała ilość próbek

--- polySmoof.R
+++ polySmoof.R
@@ -5,21 +5,29 @@ library(checkmate)
 
 source("functions.R")
 
+#Test nowych wartości
+library(pracma)
+grid_Ackley <- meshgrid(seq(-32, 32, by=0.645))
+grid_Aluffi <- meshgrid(seq(-10, 10, by=0.202))
+grid_Gold <- meshgrid(seq(-2, 2, by=0.0402))
+input_Ackley <- cbind(as.vector(grid_Ackley[[1]]), as.vector(grid_Ackley[[2]]))
+input_Aluffi <- cbind(as.vector(grid_Aluffi[[1]]), as.vector(grid_Aluffi[[2]]))
+input_Gold <- cbind(as.vector(grid_Gold[[1]]), as.vector(grid_Gold[[2]]))
+
 #SMOOF
 #TRAIN
-set.seed(9)
-arguments <- 400000
-area <- matrix(data = runif(arguments,-10,10), nrow=arguments/2, ncol=2)
+area <- input_Aluffi
 y <- generateAluffiPentini(area)
 train_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
 
 model <- lm(y~ poly(X1, X2, degree =4, raw=TRUE), data=train_data)
 
 intervals <- predict(model, interval='confidence', level = 0.99)
-MSEerr <- mse(intervals - train_data$Y) #maly blad
+MSEerr <- mse(intervals - train_data$Y) 
 
 #TEST
 set.seed(210)
+arguments <- 40000
 area2 <- matrix(data = runif(arguments,-10,10), nrow=arguments/2, ncol=2)
 y2 <- generateAluffiPentini(area2)
 test_data = data.frame(X1 = area2[,1], X2 = area2[,2], Y = y2)
@@ -32,10 +40,7 @@ summary(model)
 
 ###############################################################################################################
 #CEC2013
-set.seed(42)
-#d <- 2 #wymiary, zmiennych jest tyle ile wymiarów
-arguments <- 40000
-area <- mat #matrix(data = runif(arguments,-32,32), nrow=arguments/2, ncol=2)
+area <- input_Ackley
 y <- generateShiftedAndRotatedAckley(area)
 
 train_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
@@ -54,9 +59,7 @@ RMSEerr <- rmse(intervals_T - test_data$Y)
 
 ##########################################################################################
 #GlobalOpt - dwoch zmiennych
-set.seed(42)
-arguments <- 40000
-area <- matrix(data = runif(arguments,-2,2), nrow=arguments/2, ncol=2)
+area <- input_Gold
 y <- generateGoldPrice(area)
 
 train_data = data.frame(X1 = area[,1], X2 = area[,2], Y = y)
@@ -74,11 +77,3 @@ MSEerr_T <- mse(intervals_T - test_data$Y)
 RMSEerr <- rmse(intervals_T - test_data$Y)
 
 ##################################################################################################
-#Test nowych wartości
-library(pracma)
-grid_Ackley <- meshgrid(seq(-32, 32, by=0.645))
-grid_Aluffi <- meshgrid(seq(-10, 10, by=0.202))
-grid_Gold <- meshgrid(seq(-2, 2, by=0.0402))
-input_Ackley <- cbind(as.vector(grid_Ackley[[1]]), as.vector(grid_Ackley[[2]]))
-input_Aluffi <- cbind(as.vector(grid_Aluffi[[1]]), as.vector(grid_Aluffi[[2]]))
-input_Gold <- cbind(as.vector(grid_Gold[[1]]), as.vector(grid_Gold[[2]]))
